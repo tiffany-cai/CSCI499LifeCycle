4487b7829b4ea194dca813939f31d132a927f20a
John Solano <Jsolano.813@gmail.com>
2020-09-22 19:58:05 -0400

    added tab Bar Controller

---
+++ LifeCycle/LifeCycle/View Controllers/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  LifeCycle
+//
+//  Created by John Solano on 9/22/20.
+//  Copyright © 2020 John Solano. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBAction func EmailTextField(_ sender: UITextField) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func LoginTapped(_ sender: Any) {
+        // error, make sure everything filled out
+        
+        // DB authentication
+        
+        // Switch root controller to Tab BAr
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

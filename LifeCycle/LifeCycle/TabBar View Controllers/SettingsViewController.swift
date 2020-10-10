623204b40b5f8ac1cfd197cebff6971c426b77ad
John Solano <Jsolano.813@gmail.com>
2020-10-10 17:51:16 -0400

    Fixed bug in settings View Controller

---
+++ LifeCycle/LifeCycle/TabBar View Controllers/SettingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsViewController.swift
+//  LifeCycle
+//
+//  Created by John Solano on 10/8/20.
+//  Copyright © 2020 John Solano. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+

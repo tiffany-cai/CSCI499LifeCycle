2e7d24c5aa76b51d16c77842eba27f74822aa015
kailun ye <kailunye03@gmail.com>
2020-10-12 22:51:30 -0400

    setting page

--- LifeCycle/LifeCycle/TabBar View Controllers/SettingsViewController.swift
+++ LifeCycle/LifeCycle/TabBar View Controllers/SettingsViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 import Firebase
 
 class SettingsViewController: UIViewController {
+    @IBOutlet weak var test: UITextField!
     
+    @IBOutlet weak var settings: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+       // settings.setPadding()
+        //settings.setBottomBorder()
     }
     
 }
+/*extension UILabel {
+   /* func setPadding() {
+        let paddingview = UIView(frame:CGRect(x: 0,y: 0, width:10, height: self.frame.height))
+        self.leftView = paddingview
+        self.leftViewMode = .always
+    }*/
+    
+    func setBottomBorder() {
+        self.layer.shadowColor = UIColor.red.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0.0
+    }
+}*/
 

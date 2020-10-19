b3b94c4d31a3951718428b8b43d6c06b4969319f
John Solano <Jsolano.813@gmail.com>
2020-10-19 17:54:19 -0400

    reverted errors resolved

---
+++ LifeCycle/LifeCycle/TabBar View Controllers/SetViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  LifeCycle
+//
+//  Created by John Solano on 10/17/20.
+//  Copyright © 2020 John Solano. All rights reserved.
+//
+
+import UIKit
+
+class SetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}

4cc3d967d88b9b266b3e192ca666b07dd553efc4
John Solano <Jsolano.813@gmail.com>
2020-10-20 00:19:13 -0400

    BG color in settings + Dynamic Acc button title

--- LifeCycle/LifeCycle/TabBar View Controllers/SettingsViewControllers/AccountViewController.swift
+++ LifeCycle/LifeCycle/TabBar View Controllers/SettingsViewControllers/AccountViewController.swift
@@ -13,11 +13,13 @@ class AccountViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // Set BG
+        self.view.backgroundColor = UIColor.AppColors.AshGrey
         // Do any additional setup after loading the view.
     }
     
     @IBAction func BackBtn(_ sender: Any) {
+
         // Return to Setting Page
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let SettingsVC = storyboard.instantiateViewController(identifier: "SettingsViewController")

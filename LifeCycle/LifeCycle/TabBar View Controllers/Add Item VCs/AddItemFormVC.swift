6b41cce89b63e1e965f47c28a3d890136be9ae4e
John Solano <Jsolano.813@gmail.com>
2020-10-25 23:17:56 -0400

    More VCs added for Items tab

---
+++ LifeCycle/LifeCycle/TabBar View Controllers/Add Item VCs/AddItemFormVC.swift
@@ -0,0 +1,20 @@
+//
+//  AddItemFormVC.swift
+//  LifeCycle
+//
+//  Created by John Solano on 10/25/20.
+//  Copyright © 2020 John Solano. All rights reserved.
+//
+
+import UIKit
+
+class AddItemFormVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.AppColors.AshGrey
+    }
+    
+
+}

22125e471eff09e4b25ba54d9c6251323b11ee0f
kailun ye <kailunye03@gmail.com>
2020-10-27 01:17:17 -0400

    notification screen completed

--- LifeCycle/LifeCycle/TabBar View Controllers/SettingsViewControllers/NotificationViewController.swift
+++ LifeCycle/LifeCycle/TabBar View Controllers/SettingsViewControllers/NotificationViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class NotificationViewController: UIViewController {
-
+    @IBOutlet weak var quarter: UIButton!
+    @IBOutlet weak var half: UIButton!
+    @IBOutlet weak var full: UIButton!
+    @IBOutlet weak var none: UIButton!
+    
     /*
      
      End Dates from logged in users item table
@@ -29,7 +33,64 @@ class NotificationViewController: UIViewController {
         
     }
     
-
+    @IBAction func quarterAction(_ sender: UIButton) {
+        if sender.isSelected {
+            sender.isSelected = false
+            half.isSelected = false
+            full.isSelected = false
+            none.isSelected = false
+        }
+        else {
+            sender.isSelected = true
+            half.isSelected = false
+            full.isSelected = false
+            none.isSelected = false
+        }
+    }
+    
+    @IBAction func halfAction(_ sender: UIButton) {
+        if sender.isSelected {
+            sender.isSelected = false
+            half.isSelected = false
+            full.isSelected = false
+            none.isSelected = false
+        }
+        else {
+            sender.isSelected = true
+            quarter.isSelected = false
+            full.isSelected = false
+            none.isSelected = false
+        }
+    }
+    @IBAction func fullAction(_ sender: UIButton) {
+        if sender.isSelected {
+            sender.isSelected = false
+            quarter.isSelected = false
+            half.isSelected = false
+            none.isSelected = false
+        }
+        else {
+            sender.isSelected = true
+            quarter.isSelected = false
+            half.isSelected = false
+            none.isSelected = false
+        }
+    }
+    
+    @IBAction func noneAction(_ sender: UIButton) {
+        if sender.isSelected {
+            sender.isSelected = false
+            quarter.isSelected = false
+            half.isSelected = false
+            full.isSelected = false
+        }
+        else {
+            sender.isSelected = true
+            quarter.isSelected = false
+            half.isSelected = false
+            full.isSelected = false
+        }
+    }
     @IBAction func BackBtn(_ sender: Any) {
         print("tapped")
              //(UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
